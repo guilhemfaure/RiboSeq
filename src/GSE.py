5eae2d2f3fe218031aee2239c169586413e4bda4
guilhem <guilhem.faure@gmail.com>
2016-01-26 12:05:42 -0500

    Dump sample results and SRA link to a file and screen stats

--- src/GSE.py
+++ src/GSE.py
@@ -1,40 +1,68 @@
+__author__ = 'Guilhem Faure'
+__description__ = '''GSE.py seeks for RNA-seq and Ribo-seq experiment form a GSE number '''
+
+
 import os
-import subprocess
-
 import xml.etree.ElementTree as etree
-import string
-
-import gzip
-from gzip import GzipFile
 from urllib.request import urlopen
-import sys
-import base64
 import tarfile
-import string
+import optparse
+import sys
 
 
 class GSE:
 
-    def __init__(self, gse, d_workdir):
+    def __init__(self, gse, workdir = os.getcwd()):
         '''
 
-        :param gse:
+        :param gse: GSE identification
+        :param workdir: path to work directory
         :return:
         '''
 
         # Get variables
         self.gse = gse
-        self.d_workdir = d_workdir
+        self.workdir = os.getcwd() if workdir == None else workdir
+        self.path_xml = self.gse+'.xml'
 
         # Create Workdir
-        self.setup_dir()
+        if not os.path.exists(self.workdir):
+            self.setup_dir()
 
         # Download xml
-        self.path_xml = self.get_xml()
+        if not os.path.exists(self.path_xml):
+            self.get_xml()
 
         # Get sample name
-        self.sample = self.get_sample_from_xml()
+        self.l_sample = self.get_sample_from_xml()
 
+        # Stats and sample dumping
+        self.dump_display()
+
+        return None
+
+    def dump_display(self):
+        '''
+        Display number of sample per species
+        Dump the data into a .sample file to edit
+        :return:
+        '''
+
+        d_species = {}
+        with open(self.gse+'.sample', 'w') as fout:
+            fout.write('# Comment the sample you do not want to use\n')
+
+            for sample_name, sample_id, sample_species, sample_URL in self.l_sample:
+                if sample_species not in d_species:
+                    d_species[sample_species] = 0
+                d_species[sample_species] += 1
+                fout.write('\t'.join((sample_species, sample_name, sample_id, ' '.join(sample_URL)))+'\n')
+
+        for species in d_species:
+            print(species, d_species[species])
+
+        print('Total number of sample', len(self.l_sample))
+        print('Edit your sample file, comment the samples', self.gse+'.sample')
 
 
     def setup_dir(self):
@@ -44,7 +72,7 @@ class GSE:
         :return:
         '''
 
-        path_root = os.path.join(self.d_workdir, self.gse)
+        path_root = os.path.join(self.workdir, self.gse)
         if not os.path.isdir(path_root):
             os.makedirs(path_root)
             print('Create workdir', path_root)
@@ -84,32 +112,29 @@ class GSE:
 
         return self.gse+'.xml'
 
-    def get_sample_from_xml(self, condition=lambda x: True):
+    def get_sample_from_xml(self):
         '''Adapted from JHussmann
-        Parse an xml file describing a GEO accession number to extract samples.
-
-        Sanitizes the names of samples by replacing spaces, slashes, and brackets
-        with underscores, replacing unicode deltas with the string 'delta_', and
-        removing parentheses.
-
-        condition -- a filtering function to be applied to sample names
-
-        Returns a list of (sample name, list of URLs) tuples.
+        Get data from th xml
+        Returns a list of (sample_name, sample_id, sample_species, sample_URL) tuples.
         '''
 
 
-        samples = []
+        l_sample = []
 
         tree = etree.parse(self.gse+'.xml')
         root = tree.getroot()
         for child in root:
 
             if child.tag.endswith('Sample'):
-
+                sample_id = child.attrib['iid']
+                sample_URL = []
+                sample_name = None
+                sample_species = None
                 for grand in child:
+
                     if grand.tag.endswith('Title'):
                         sample_name = grand.text
-                        sample_URLs = []
+
                         if not type(sample_name) == str:
                             # Some sample names (knockout strains) have a unicode
                             # delta in them. Replace this with a spelled out
@@ -119,17 +144,30 @@ class GSE:
                                 sample_name = sample_name.replace(delta, 'delta_')
                             sample_name = str(sample_name)
 
+
                     elif grand.tag.endswith('Supplementary-Data') and grand.attrib['type'] == 'SRA Experiment':
-                        sample_URL = grand.text.strip()
-                        sample_URLs.append(sample_URL)
+                        sample_URL.append(grand.text.strip())
 
-                samples.append((sample_name, sample_URLs))
+                    # Grab the species
+                    elif grand.tag.endswith('Channel'):
+                        for great in grand:
+                            if great.tag.endswith('Organism'):
+                                sample_species = great.text.strip()
+                l_sample.append((sample_name, sample_id, sample_species, sample_URL))
 
-        print('Number of sample', len(samples))
-        return samples
+
+        return l_sample
 
 
 
 if __name__ == '__main__':
 
-    GSE('GSE67387', '../../../RiboSeq_dev')
+    parser = optparse.OptionParser()
+    parser.add_option('-g', '--gse', dest='gse', help='GSE identification')
+    parser.add_option('-w', '--workdir', dest='workdir', help='Workdir, where .xml and .sample will be generated', default=None)
+    (options, args) = parser.parse_args()
+
+    if options.gse == None:
+        parser.error("You should provide a GSE number with -g or --gse")
+
+    GSE(options.gse, options.workdir)
