8dbb27785b05726238e777c89d6f043e212fb631
Guilhem Faure <guilhem@Tatianas-MacBook-Air.local>
2016-03-25 12:55:12 -0400

    documentation

--- src/Genbank.py
+++ src/Genbank.py
@@ -1,5 +1,20 @@
 __author__ = 'Guilhem Faure'
-__description__ = '''Gather Genbank from the genome assembly id'''
+__description__ = '''Managing Genbank from the genome assembly id
+Biopython and python3 are required
+
+<Script mode>
+Get genbank file
+python3 Genbank.py -g NC_000913.3 [-o NC_000913.3.gb]
+
+Get 16s
+python3 Genbank.py -g NC_000913.3 -m 16s.fasta
+
+<Class mode>
+myGB = Genbank(id = 'NC_000913.3')
+myGB.   extract_16s('output.file')
+        save_genbank('output.gb')
+
+'''
 
 import optparse
 import sys
@@ -11,7 +26,11 @@ class Genbank:
     def __init__(self, **kwargs):
         '''
 
-        :param id:
+        :param kwargs: id -> chromosome version (required)
+
+                        <Script Mode>
+                       dump -> dump genbank file in script mode
+                       rRNAS -> dump 16s fasta sequence in script mode
         :return:
         '''
 
@@ -20,16 +39,29 @@ class Genbank:
             self.id = kwargs.get('id')
             self._handle = self._download_gb()
 
+
+        ## Script MODE
         # If dump, we dump in the output
         if kwargs.get('dump'):
             self.dump_gb(kwargs.get('dump'))
             return None
 
+        # Extract 16S into a file
+        if kwargs.get('rRNA16s'):
+            self._load_gb()
+            self.extract_16S(kwargs.get('rRNA16s'))
+            return None
+
+
+        ## Class mode
         # Create a _gb variable
         self._load_gb()
 
         # Extract annotated 16S
-        self.extract_16S()
+        #self.extract_16S(output = None)
+
+        # Save genbank
+        #self.save_genbank(output = 'output.genbank')
 
     def _download_gb(self):
         '''
@@ -45,7 +77,8 @@ class Genbank:
 
     def dump_gb(self, p_output):
         '''
-        Write the Genbank file
+        <Script Mode>
+        Write the Genbank file from the script mode
         :param p_output: output path for genbank
         :return:
         '''
@@ -53,6 +86,8 @@ class Genbank:
         with open(p_output, 'w') as fout:
             fout.write(self._handle.read())
 
+
+        print ('Genbank available at: ', p_output)
         #self._handle.close()
 
         return
@@ -65,12 +100,20 @@ class Genbank:
 
         self._gb = SeqIO.read(self._handle, 'genbank')
 
-    def extract_16S(self):
+        return
+
+
+    def extract_16S(self, output = None):
+        '''
+        <Class mode or Script mode>
+        Extract 16S fasta from the genbank annotation
+        if output is specified it dumps the sequences into a file
+        :return: d_seq [header] -> sequence
         '''
 
-        :return:
-        '''
-
+        if output:
+            fout = open(output, 'w')
+        d_seq = {}
         fasta_format = '>{type}|{genome}|' \
                        'position={start}-{stop}:{strand}' \
                        '|locus={locus}' \
@@ -97,16 +140,37 @@ class Genbank:
                         d_info['strand']    = gene.location.strand
                         d_info['product']   = ','.join(gene.qualifiers['product'])
 
-                        print (fasta_format.format(**d_info))
+                        str_fasta = fasta_format.format(**d_info)
 
-        print (self._gb.annotations)
-        print (self._gb.dbxrefs)
-        print (self._gb.description)
-        print (self._gb.features)
-        print (self._gb.id)
-        print (self._gb.name)
+                        header, seq, _ = str_fasta.split('\n')
+                        d_seq[header] = seq
+
+                        if output:
+                            fout.write(str_fasta)
+
+        if output:
+            fout.close()
+
+        return d_seq
+        # print (self._gb.annotations)
+        # print (self._gb.dbxrefs)
+        # print (self._gb.description)
+        # print (self._gb.features)
+        # print (self._gb.id)
+        # print (self._gb.name)
+
+    def save_genbank(self, output):
+        '''
+        <Class mode>
+        Save genbank into a file
+        :return:
+        '''
 
 
+        with open("short_seqs.fasta", "w") as output:
+            SeqIO.write(self._gb, output, "genbank")
+
+        return
 
 
 if __name__ == '__main__':
@@ -115,16 +179,21 @@ if __name__ == '__main__':
     parser = optparse.OptionParser(usage = usage)
     parser.add_option('-g', '--genbank', dest='genbank', help='Genbank file full size', default = None)
     parser.add_option('-o', '--output', dest='output', help='output file', default = None)
+    parser.add_option('-m', '--r16s', dest='rRNA16s', help='dump 16S fasta', default = None)
     (options, args) = parser.parse_args()
 
 
     if options.genbank is None:
-        parser.error('You should provide a name assembly i.e. NC_000913 see -h')
+        parser.error('You should provide a name assembly i.e. NC_000913.3 see -h')
 
     if options.genbank and options.output:
         MyGB = Genbank(id = options.genbank, dump = options.output)
         sys.exit('Genbank:'+options.genbank+' available at:'+options.output)
 
+    if options.genbank and options.rRNA16s:
+        MyGB = Genbank(id = options.genbank, rRNA16s = options.rRNA16s)
+        sys.exit('Genbank:'+options.genbank+' available at:'+options.rRNA16s)
+
     if options.genbank:
-        MyGB = Genbank(id = options.genbank)
+        MyGB = Genbank(id = options.genbank, dump = options.genbank+'.gb')
 
